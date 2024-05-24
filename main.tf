f322bae6d474d30c44e59ec83cdf096e30866a0d
arunpanagatla1 <arunpanagatla1@gmail.com>
2024-05-24 13:54:17 +0530

    update

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0440d3b780d96b29d"
-    instance_type = "t2.nano"
+    ami = "ami-0cc9838aa7ab1dce7"
+    instance_type = "t2.micro"
     tags = {
-      Name = "dev-ec2"
+      Name = "jenkins"
     }
 }
