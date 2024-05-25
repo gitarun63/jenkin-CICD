849b27bba33ef63cffc6170c64ea55083829bf70
arunpanagatla1 <arunpanagatla1@gmail.com>
2024-05-25 15:19:09 +0530

    updates

--- main.tf
+++ main.tf
@@ -2,10 +2,26 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-0cc9838aa7ab1dce7"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "jenkins"
-    }
+resource "aws_s3_bucket" "rmbucket123" {
+  bucket = "statefilestore"
+}
+
+
+resource "aws_dynamodb_table" "DynamoStateFileLock" {
+  name = "statefilelock"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "remote_bucket_versioning" {
+  bucket = aws_s3_bucket.rmbucket123.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
